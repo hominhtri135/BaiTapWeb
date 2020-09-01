4150d2cc2e7f8c269ac4092b9547a50111b626e0
hominhtri135 <hominhtri135@github.com>
2020-09-01 09:59:23 +0700

    done

---
+++ ch02_ex1/display_discount.php
@@ -0,0 +1,37 @@
+<?php 
+    $product_description = $_POST['product_description'];
+    $list_price = $_POST['list_price'];
+    $discount_percent = $_POST['discount_percent'];
+    
+    $discount = $list_price * $discount_percent * .01;
+    $discount_price = $list_price - $discount;
+    
+    
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <main>
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo '$'.$product_description; ?></span><br>
+
+        <label>List Price:</label>
+        <span><?php echo '$'.$list_price; ?></span><br>
+
+        <label>Standard Discount:</label>
+        <span><?php echo $discount_percent.'%'; ?></span><br>
+
+        <label>Discount Amount:</label>
+        <span><?php echo '$'.$discount; ?></span><br>
+
+        <label>Discount Price:</label>
+        <span><?php echo '$'.$discount_price; ?></span><br>
+    </main>
+</body>
+</html>
